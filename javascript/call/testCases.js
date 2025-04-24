3571e0433c6f5d263db83643ea17a092f3a0a96b
Apurva Wadekar <apurva.w17@gmail.com>
2025-04-24 09:33:09 +0530

    fix: call testcases

---
+++ javascript/call/testCases.js
@@ -0,0 +1,27 @@
+import './call';
+
+describe('Function.prototype.myCall', () => {
+  const person = {
+    name: 'John',
+  };
+
+  function getName(this) {
+    return this.name;
+  }
+
+  function sum(...args) {
+    return args.reduce((acc, num) => acc + num, 0);
+  }
+
+  test('Function.prototype.myCall is a function', () => {
+    expect(typeof Function.prototype.myCall).toBe('function');
+  });
+
+  test('`this` is bound', () => {
+    expect(getName.myCall(person)).toStrictEqual('John');
+  });
+
+  test('with a parameter', () => {
+    expect(sum.myCall(null, 1)).toBe(1);
+  });
+});
